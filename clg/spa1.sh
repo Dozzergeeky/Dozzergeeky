3f96cca687854f9fc2724aa0de33d1149a43587d
Dozzergeeky <d2b4mb@gmail.com>
2023-11-25 21:26:51 +0530

    dot files

--- clg/spa1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 1. Write a program to print the process id and parent process id.
-
-# Use the getpid() and getppid() functions to get the process id and parent process id
-# Reference: [1]
-echo "The process id of this script is $$"
-echo "The parent process id of this script is $PPID"
