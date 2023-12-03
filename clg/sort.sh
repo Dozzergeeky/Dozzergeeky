2a7c1a8931acb2f015d7432049270953a1fa7cae
Dozzergeeky <d2b4mb@gmail.com>
2023-12-03 21:59:02 +0530

    optimising

--- clg/sort.sh
+++ clg/sort.sh
@@ -2,7 +2,5 @@
 # This is a shell script to sort an array of numbers in ascending order
 echo "Enter the numbers separated by spaces: "
 read -a array # Read the numbers from the user and store them in an array
-IFS=$'\n' sorted=($(sort -n <<<"${array[*]}")) # Use sort to sort the array numerically and store the result in another array
-unset IFS # Reset the IFS to the default value
+sorted=($(printf '%s\n' "${array[@]}" | sort -n)) # Sort the array
 echo "The sorted array is: ${sorted[*]}" # Print the sorted array
-
